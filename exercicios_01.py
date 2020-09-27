28e2dc79f677e2e6e1989fe7a3feb146b8817c63
Altieris Peixoto <altieris.marcelino@gmail.com>
2020-09-27 14:41:23 -0300

    add exercicios aula 01

---
+++ exercicios_01.py
@@ -0,0 +1,9 @@
+## 1 -  crie 3 variaveis (númerica, string e booleana)
+
+## 2 - crie uma lista de animais, adicione um animal e remova um animal da lista.
+## Use o google para verificar como se remove um item da lista
+
+## 3 - com a lista de animais criada no exercicio anterior, crie uma nova lista de tamanho dos animais e percorra os animais utilizando
+## o zip e o loop for .
+
+## imprima somente os animais que sao grandes utilizando o if / else:
